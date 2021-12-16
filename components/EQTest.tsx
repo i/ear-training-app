60668a7c349f7704bc1d7b915476ce95ab455f89
Ian Lozinski <ilozinski@spotify.com>
2021-12-16 15:02:20 -0600

    wip

--- components/EQTest.tsx
+++ components/EQTest.tsx
@@ -4,6 +4,7 @@ import { Text, View } from '../components/Themed';
 
 import { MultiAudioPlayer } from '../components/AudioPlayer';
 import { Api } from '../api/api';
+import Slider from '@react-native-community/slider';
 
 export function EQTest() {
   const [text, onChangeText] = React.useState('Enter frequency');
@@ -30,14 +31,43 @@ export function EQTest() {
   }
 
   React.useEffect(getTest, []);
-  React.useEffect(showTestItem, [testItems])
+  React.useEffect(showTestItem, [testItems]);
+
+  const [freq, setFreq] = React.useState(500);
+  const onTouchMove = function(val) {
+    setFreq(val);
+  }
+
+  function logslider(position: number) {
+    // position will be between 0 and 100
+    var minp = 0.0;
+    var maxp = 1.0;
+
+    var minv = Math.log(20);
+    var maxv = Math.log(20000);
+
+    // calculate adjustment factor
+    var scale = (maxv-minv) / (maxp-minp);
+
+    return Math.ceil(Math.exp(minv + scale*(position-minp)));
+  }
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Enter the boosted frequency!</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <MultiAudioPlayer uris={uris} />
-      <TextInput value={text}  onChangeText={onChangeText} />
+
+      <Text>{freq}</Text>
+      <Slider
+        style={styles.slider}
+        minimumValue={20}
+        maximumValue={20000}
+        minimumTrackTintColor="#FFF000"
+        maximumTrackTintColor="#000000"
+        onValueChange={onTouchMove}
+      />
+
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
     </View>
   )
 }
@@ -56,5 +86,9 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  slider: {
+    width: '80%',
+    height: 1,
   },
 });
